4ef056f0ca719aa8b3c32dbe12b78527947fc769
emena <menaericdaniel@gmail.com>
2026-09-08 22:38:17 -0300

    Enhance patient treatment analysis program by refining data input validation checks and optimizing performance metrics for improved accuracy in outcome assessment

---
+++ activity_7_diccionarios_2.py
@@ -0,0 +1,41 @@
+# Un arquitecto nos pidió que transformemos el siguiente plano en código Python, utilizando diccionarios.
+#
+#  📌 Pasos:
+# 
+# 1.Crear un diccionario para cada departamento (departamento1, departamento2, etc.).
+#   Cada diccionario debe tener las siguientes claves:
+#   - "ambientes"
+#   - "metros2"
+#   - "piso"
+# 2. Al finalizar, mostrar cada diccionario por consola para verificar su contenido.
+
+# Crear departamento1, departamento2, departamento3 y departamento4.
+departamento1 = {
+    "ambientes": 2,
+    "metros2": 100,
+    "piso": 1
+}
+
+departamento2 = {
+    "ambientes": 3,
+    "metros2": 150,
+    "piso": 2
+}
+
+departamento3 = {
+    "ambientes": 4,
+    "metros2": 200,
+    "piso": 3
+}
+
+departamento4 = {
+    "ambientes": 1,
+    "metros2": 50,
+    "piso": 4
+}
+
+# Al finalizar, mostrar cada diccionario por consola para verificar su contenido.
+print(departamento1)
+print(departamento2)
+print(departamento3)
+print(departamento4)
